b54a6d7e183dfa9f991201e3fdbf6ab503461587
itamarlebel <itamarl@deliveright.com>
2024-11-05 14:49:16 +0200

    made a method for updating the rabbit mq exchange values

--- src/controllers/configCtrl.js
+++ src/controllers/configCtrl.js
@@ -1,5 +1,6 @@
 const configs = require('../data/configs.json');
-const {changeDb, checkIfDbExists} = require("../db");
+const {changeDb } = require("../db");
+const {updateUrls, updateRabbitExchangeData} = require("../utils");
 
 exports.getConfig = async (db) => {
     return await db.collection('config').findOne({})
@@ -10,10 +11,19 @@ const updateConfig = async (db, updatedFields) => {
     console.log(`config successfully updated`)
 }
 
+const updateConfigFields = (dbName) => {
+    const config = configs[dbName]
+    console.log(config)
+    updateUrls(config, dbName)
+    updateRabbitExchangeData(config)
+    return config
+}
+
 exports.updateConfigForDB = async (conn, dbName) => {
     try {
         const db = await changeDb(conn, dbName);
-        await updateConfig(db, configs[dbName])
+        const newConfig = updateConfigFields(dbName)
+        await updateConfig(db, newConfig)
         console.log(`Config updated to ${dbName}`)
     } catch (err) {
         console.error(`failed to update the config of DB ${dbName}: ${err}`);
--- src/utils.js
+++ src/utils.js
@@ -7,17 +7,37 @@ const getUrlByDbName = (dbName) => {
     return process.env[envVarName]
 }
 
+const setUrlForKey = (confObj, key, defaultUrl) => {
+    switch (key) {
+        case 'globalSync.url':
+            // If the key is "globalSync.url" then we always should put the drl url because it's the master of the envs.
+            return process.env.E2E_DRL_URL
+        case 'globalSync.rabbitmq.connection_url':
+            // If this is the key, need to check if it's the master
+            return (confObj["globalSync.is_master"] === true) ? process.env.RABBITMQ_URL : null
+        default:
+            return defaultUrl
+    }
+}
+
 exports.updateUrls = (confObject, dbName) => {
-    const newUrl = getUrlByDbName(dbName)
+    const defaultUrl = getUrlByDbName(dbName)
     Object.keys(confObject).forEach(key => {
         if (fieldsWithUrls.includes(key)) {
-            // If the key is "globalSync.url" then we always should put the drl url because it's the master of the envs.
-            if (key === "globalSync.url") {
-                confObject[key] = process.env.E2E_DRL_URL;
-            } else {
-                confObject[key] = newUrl;
+            const url = setUrlForKey(confObject, key, defaultUrl)
+            if (url === null) {
+                throw new Error(`URL is null, please check for issues`)
             }
+            confObject[key] = url;
         }
     })
-    return confObject
+}
+
+exports.updateRabbitExchangeData = (confObj) => {
+    if (confObj["globalSync.is_master"] === true) {
+        confObj["globalSync.rabbitmq.exchange_name"] = process.env.RABBITMQ_EXCHANGE
+        confObj["globalSync.rabbitmq.master_queue"] = process.env.RABBITMQ_MASTER_QUEUE
+        return
+    }
+    console.log("If you see this log then this conf file is for no rabbit master")
 }
